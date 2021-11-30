cdc8b9543838324983ff78d3dae5ad644a7eeca5
cheeck <vova.rupasov8131@gmail.com>
2021-11-30 16:32:44 +0300

    NewBid component changes

--- src/components/NewBid/NewBid.tsx
+++ src/components/NewBid/NewBid.tsx
@@ -1,14 +1,26 @@
 import { FC, useState } from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import {
+  Form,
+  Row,
+  Col,
+  Button,
+  ButtonGroup,
+  InputGroup,
+} from "react-bootstrap";
 import style from "./NewBid.module.scss";
 import PhoneInput from "../Inputs/PhoneInput";
 import InsuranseInput from "../Inputs/InsuranseInput";
+import {
+  BID_CREATE_PRODUCT_LIST,
+  BID_CREATE_STATUS_LIST,
+} from "../../constants/bidCreate";
+import React from "react";
 
 class FormParams {
   phone = ""; // номер телефона
-  firstname = ""; // имя
-  lastname = ""; // фамилия
-  secondname = ""; // отчество
+  firstName = ""; // имя
+  lastName = ""; // фамилия
+  secondName = ""; // отчество
   status = ""; // статус лица
   dateOfBirth = ""; // дата рождения
   region = ""; // регион
@@ -27,8 +39,10 @@ const NewBid: FC = () => {
   return (
     <div className={style.newBid}>
       <Row>
-        <Col lg="5">
+        <Col lg={5}>
           <div className={style.main}>
+            <h4>Создание заявки</h4>
+            <hr />
             <div className={style.element}>
               <Form.Label>Телефон</Form.Label>
               <PhoneInput
@@ -41,19 +55,20 @@ const NewBid: FC = () => {
               <Form.Label>Фамилия</Form.Label>
               <Form.Control
                 placeholder="Фамилия"
-                value={state.lastname}
+                value={state.lastName}
                 onChange={(e) =>
-                  setState({ ...state, lastname: e.target.value })
+                  setState({ ...state, lastName: e.target.value })
                 }
               />
             </div>
+
             <div className={style.element}>
               <Form.Label>Имя</Form.Label>
               <Form.Control
                 placeholder="Имя"
-                value={state.firstname}
+                value={state.firstName}
                 onChange={(e) =>
-                  setState({ ...state, firstname: e.target.value })
+                  setState({ ...state, firstName: e.target.value })
                 }
               />
             </div>
@@ -62,9 +77,9 @@ const NewBid: FC = () => {
               <Form.Label>Отчество</Form.Label>
               <Form.Control
                 placeholder="Отчество"
-                value={state.secondname}
+                value={state.secondName}
                 onChange={(e) =>
-                  setState({ ...state, secondname: e.target.value })
+                  setState({ ...state, secondName: e.target.value })
                 }
               />
             </div>
@@ -106,12 +121,24 @@ const NewBid: FC = () => {
 
             <div className={style.element}>
               <Form.Label>Срок займа</Form.Label>
-              <Form.Control placeholder="Срок займа от 7 до 180 для МФО" />
+              <Form.Control
+                placeholder="Срок займа от 7 до 180 для МФО"
+                value={state.creditTime}
+                onChange={(e) =>
+                  setState({ ...state, creditTime: e.target.value })
+                }
+              />
             </div>
 
             <div className={style.element}>
               <Form.Label>Сумма кредита</Form.Label>
-              <Form.Control placeholder="Сумма кредита от 1000 до 100,000 для МФО" />
+              <Form.Control
+                placeholder="Сумма кредита от 1000 до 100,000 для МФО"
+                value={state.creditAmount}
+                onChange={(e) =>
+                  setState({ ...state, creditAmount: e.target.value })
+                }
+              />
             </div>
 
             <div className={style.element}>
@@ -121,8 +148,39 @@ const NewBid: FC = () => {
           </div>
         </Col>
 
-        <Col>
-          <div className={style.productList}>123</div>
+        <Col md={7}>
+          <div className={style.productList}>
+            <h4>Оформленные заявки</h4>
+            <p>Не указан номер телефона</p>
+
+            <hr />
+            <div className={style.grid}>
+              <h4>Выберите продукт</h4>
+              <h4>Выберите статус</h4>
+              <div>
+                {BID_CREATE_PRODUCT_LIST.map(({ name, id }) => (
+                  <InputGroup size="sm" key={id}>
+                    <InputGroup.Radio />
+                    <InputGroup.Text>{name}</InputGroup.Text>
+                  </InputGroup>
+                ))}
+              </div>
+              <div>
+                {BID_CREATE_STATUS_LIST.map(({ name, id }) => (
+                  <InputGroup size="sm" key={id}>
+                    <InputGroup.Radio />
+                    <InputGroup.Text>{name}</InputGroup.Text>
+                  </InputGroup>
+                ))}
+              </div>
+            </div>
+
+            <hr />
+
+            <Form.Control placeholder="Коммеентарий" />
+
+            <Button className={style.marginTop}>Сохранить</Button>
+          </div>
         </Col>
       </Row>
     </div>
