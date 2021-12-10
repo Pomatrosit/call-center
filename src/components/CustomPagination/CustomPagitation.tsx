0ba5ba87e6d65b7a7f3c231143ceb8bf0d719960
Vasilii Iurkin <Vasilii_Iurkin@epam.com>
2021-12-10 08:48:44 +0300

    bid actions changes

--- src/components/CustomPagination/CustomPagitation.tsx
+++ src/components/CustomPagination/CustomPagitation.tsx
@@ -1,99 +1,173 @@
-import { FC, Dispatch, SetStateAction } from 'react'
-import { Pagination } from 'react-bootstrap'
+import { FC } from "react";
+import { Pagination } from "react-bootstrap";
 
 interface ICustomPaginationProps {
-    pageCount: number
-    activePage: number
-    setActivePage: Dispatch<SetStateAction<number>>
+  pageCount: number;
+  activePage: number;
+  setActivePage: (page: number) => void;
 }
 
-const CustomPagination: FC<ICustomPaginationProps> = ({ activePage, setActivePage, pageCount }) => {
-    
-  let pages = []
+const CustomPagination: FC<ICustomPaginationProps> = ({
+  activePage,
+  setActivePage,
+  pageCount,
+}) => {
+  let pages = [];
 
-  if (pageCount <= 10){
+  if (pageCount <= 10) {
     for (let number = 1; number <= pageCount; number++) {
-        pages.push(
-          <Pagination.Item key={number} active={number === activePage} onClick={() => setActivePage(number)}>
-            {number}
-          </Pagination.Item>,
-        );
+      pages.push(
+        <Pagination.Item
+          key={number}
+          active={number === activePage}
+          onClick={() => setActivePage(number)}
+        >
+          {number}
+        </Pagination.Item>
+      );
     }
   } else {
-    if (activePage === 1){
-        pages.push(<Pagination.Item key={1} active={true}>1</Pagination.Item>)
-        pages.push(<Pagination.Item key={2} onClick={() => setActivePage(2)}>2</Pagination.Item>)
-        pages.push(<Pagination.Ellipsis key="ellipsis1"/>)
-        pages.push(<Pagination.Item key={pageCount} onClick={() => setActivePage(pageCount)}>
-            {pageCount}
-        </Pagination.Item>)
-    }
-    else if (activePage === 2) {
-        pages.push(<Pagination.Item key={1} onClick={() => setActivePage(1)}>1</Pagination.Item>)
-        pages.push(<Pagination.Item key={2} active={true}>2</Pagination.Item>)
-        pages.push(<Pagination.Item key={3} onClick={() => setActivePage(3)}>3</Pagination.Item>)
-        pages.push(<Pagination.Ellipsis key="ellipsis1"/>)
-        pages.push(<Pagination.Item key={pageCount} onClick={() => setActivePage(pageCount)}>
-            {pageCount}
-        </Pagination.Item>)
-    }
-    else if (activePage === pageCount) {
-        pages.push(<Pagination.Item key={1} onClick={() => setActivePage(1)}>1</Pagination.Item>)
-        pages.push(<Pagination.Ellipsis key="ellipsis1"/>)
-        pages.push(
-            <Pagination.Item key={pageCount - 1} onClick={() => setActivePage(pageCount - 1)}>
-                {pageCount - 1}
-            </Pagination.Item>
-        )
-        pages.push(<Pagination.Item key={pageCount} active={true}>{pageCount}</Pagination.Item>)
-    }
-    else if (activePage === pageCount - 1) {
-        pages.push(<Pagination.Item key={1} onClick={() => setActivePage(1)}>1</Pagination.Item>)
-        pages.push(<Pagination.Ellipsis key="ellipsis1"/>)
-        pages.push(
-            <Pagination.Item key={pageCount - 2} onClick={() => setActivePage(pageCount - 2)}>
-                {pageCount - 2}
-            </Pagination.Item>
-        )
-        pages.push(
-            <Pagination.Item key={pageCount - 1} active = {true}>
-                {pageCount - 1}
-            </Pagination.Item>
-        )
-        pages.push(<Pagination.Item key={pageCount} onClick={() => setActivePage(pageCount)}>{pageCount}</Pagination.Item>)
-    }
-    else {
-        pages.push(<Pagination.Item key={1} onClick={() => setActivePage(1)}>1</Pagination.Item>)
-        if (activePage !== 3) pages.push(<Pagination.Ellipsis key="ellipsis1"/>)
-        pages.push(
-        <Pagination.Item 
-            key={activePage - 1}
-            onClick={() => setActivePage(activePage - 1)}
+    if (activePage === 1) {
+      pages.push(
+        <Pagination.Item key={1} active={true}>
+          1
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item key={2} onClick={() => setActivePage(2)}>
+          2
+        </Pagination.Item>
+      );
+      pages.push(<Pagination.Ellipsis key="ellipsis1" />);
+      pages.push(
+        <Pagination.Item
+          key={pageCount}
+          onClick={() => setActivePage(pageCount)}
         >
-            {activePage - 1}
-        </Pagination.Item>)
-        pages.push(<Pagination.Item key={activePage} active={true}>{activePage}</Pagination.Item>)
-        pages.push(
-        <Pagination.Item 
-            key={activePage + 1}
-            onClick={() => setActivePage(activePage + 1)}
+          {pageCount}
+        </Pagination.Item>
+      );
+    } else if (activePage === 2) {
+      pages.push(
+        <Pagination.Item key={1} onClick={() => setActivePage(1)}>
+          1
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item key={2} active={true}>
+          2
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item key={3} onClick={() => setActivePage(3)}>
+          3
+        </Pagination.Item>
+      );
+      pages.push(<Pagination.Ellipsis key="ellipsis1" />);
+      pages.push(
+        <Pagination.Item
+          key={pageCount}
+          onClick={() => setActivePage(pageCount)}
         >
-            {activePage + 1}
-        </Pagination.Item>)
-        if (activePage !== pageCount - 2) pages.push(<Pagination.Ellipsis key="ellipsis2"/>)
-        pages.push(<Pagination.Item key={pageCount} onClick={() => setActivePage(pageCount)}>
-            {pageCount}
-        </Pagination.Item>)
+          {pageCount}
+        </Pagination.Item>
+      );
+    } else if (activePage === pageCount) {
+      pages.push(
+        <Pagination.Item key={1} onClick={() => setActivePage(1)}>
+          1
+        </Pagination.Item>
+      );
+      pages.push(<Pagination.Ellipsis key="ellipsis1" />);
+      pages.push(
+        <Pagination.Item
+          key={pageCount - 1}
+          onClick={() => setActivePage(pageCount - 1)}
+        >
+          {pageCount - 1}
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item key={pageCount} active={true}>
+          {pageCount}
+        </Pagination.Item>
+      );
+    } else if (activePage === pageCount - 1) {
+      pages.push(
+        <Pagination.Item key={1} onClick={() => setActivePage(1)}>
+          1
+        </Pagination.Item>
+      );
+      pages.push(<Pagination.Ellipsis key="ellipsis1" />);
+      pages.push(
+        <Pagination.Item
+          key={pageCount - 2}
+          onClick={() => setActivePage(pageCount - 2)}
+        >
+          {pageCount - 2}
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item key={pageCount - 1} active={true}>
+          {pageCount - 1}
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item
+          key={pageCount}
+          onClick={() => setActivePage(pageCount)}
+        >
+          {pageCount}
+        </Pagination.Item>
+      );
+    } else {
+      pages.push(
+        <Pagination.Item key={1} onClick={() => setActivePage(1)}>
+          1
+        </Pagination.Item>
+      );
+      if (activePage !== 3) pages.push(<Pagination.Ellipsis key="ellipsis1" />);
+      pages.push(
+        <Pagination.Item
+          key={activePage - 1}
+          onClick={() => setActivePage(activePage - 1)}
+        >
+          {activePage - 1}
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item key={activePage} active={true}>
+          {activePage}
+        </Pagination.Item>
+      );
+      pages.push(
+        <Pagination.Item
+          key={activePage + 1}
+          onClick={() => setActivePage(activePage + 1)}
+        >
+          {activePage + 1}
+        </Pagination.Item>
+      );
+      if (activePage !== pageCount - 2)
+        pages.push(<Pagination.Ellipsis key="ellipsis2" />);
+      pages.push(
+        <Pagination.Item
+          key={pageCount}
+          onClick={() => setActivePage(pageCount)}
+        >
+          {pageCount}
+        </Pagination.Item>
+      );
     }
   }
 
   return (
     <Pagination>
-        <Pagination.First onClick={() => setActivePage(1)}/>
-        {pages}
-        <Pagination.Last onClick={() => setActivePage(pageCount)}/>
+      <Pagination.First onClick={() => setActivePage(1)} />
+      {pages}
+      <Pagination.Last onClick={() => setActivePage(pageCount)} />
     </Pagination>
-  )
-}
+  );
+};
 
-export default CustomPagination
+export default CustomPagination;
