24da8114508c88053498db79334a342c5a16fee2
Pomatrosit <timbersaw442211@gmail.com>
2021-12-24 12:26:31 +0300

    web phone layout

--- src/components/CallSection/CallSection.tsx
+++ src/components/CallSection/CallSection.tsx
@@ -1,11 +1,22 @@
-import { Button } from "react-bootstrap"
-import { FC } from "react"
-import classes from "./CallSection.module.scss"
+import { Button } from "react-bootstrap";
+import { FC } from "react";
+import classes from "./CallSection.module.scss";
+import { openModal } from "../../store/modal/actions";
+import { useDispatch } from "react-redux";
+import WebPhone from "../WebPhone/WebPhone";
 
 const CallSection: FC = () => {
+  const dispatch = useDispatch();
+
+  const onWebPhoneClick = () => {
+    dispatch(openModal("Веб-телефон", WebPhone));
+  };
+
   return (
     <div className={classes.callSection}>
-      <Button variant='success'>Телефон</Button>
+      <Button onClick={onWebPhoneClick} variant="success">
+        Телефон
+      </Button>
       <div className={classes.callInfo}>
         <p className={classes.callInfo__status}>Ожидание вызова</p>
         <p className={classes.callInfo__time}>
@@ -13,7 +24,7 @@ const CallSection: FC = () => {
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CallSection
+export default CallSection;
--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,26 +1,26 @@
-import { Modal } from 'react-bootstrap'
-import { useDispatch } from 'react-redux'
-import { useAppSelector } from '../../hooks/useAppSelector'
-import { closeModal } from '../../store/modal/actions'
+import { Modal } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import { closeModal } from "../../store/modal/actions";
 
 const ModalWindow = () => {
+  const { show, title, Component } = useAppSelector((state) => state.modal);
 
-    const { show, title, Component } = useAppSelector(state => state.modal)
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-
-    if (show) return (
+  if (show)
+    return (
       <div>
-        <Modal show={show} onHide={() => dispatch(closeModal())}>
+        <Modal size="lg" show={show} onHide={() => dispatch(closeModal())}>
           <Modal.Header closeButton>
-            <Modal.Title>{ title }</Modal.Title>
+            <Modal.Title>{title}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>{ Component && <Component /> }</Modal.Body>
+          <Modal.Body>{Component && <Component />}</Modal.Body>
         </Modal>
       </div>
-    )
+    );
 
-    return null
-}
+  return null;
+};
 
-export default ModalWindow
+export default ModalWindow;
