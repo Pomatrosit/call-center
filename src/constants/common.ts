e9e56d8b922098d38beb4683ef3ffc6d61e6455b
Vasilii Iurkin <vasiliiiurkin@MacBook-Air-Vasilii.local>
2022-04-12 08:33:51 +0300

    web-phone

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,6 @@
 export const API_URL = "https://lk.sipli.ru/cc";
 // export const SOCKET_URL = "https://lk.sipli.ru/ccws/";
-export const SOCKET_URL = "wss://lk.sipli.ru:4001";
+export const SOCKET_URL = "https://lk.sipli.ru:4001";
 
 export enum TOKENS {
   accessToken = "accessToken",
