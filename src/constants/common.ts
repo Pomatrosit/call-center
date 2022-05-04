044a8db5ce2fa4e89ce935bfbc796f9c3af0e4c5
Vasilii Iurkin <vasiliiiurkin@MacBook-Air-Vasilii.local>
2022-05-04 22:06:17 +0300

    api urls

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,9 +1,9 @@
-// export const API_URL = "https://lk.sipli.ru/cc";
-// export const SOCKET_URL = "https://lk.sipli.ru:4001";
-
 /// DEV
-export const API_URL = "https://lk.sipli.ru:3057";
-export const SOCKET_URL = "https://lk.sipli.ru:4011";
+export const API_URL = "https://lk.sipli.ru/cc";
+export const SOCKET_URL = "https://lk.sipli.ru:4001";
+
+// export const API_URL = "https://lk.sipli.ru:3057";
+// export const SOCKET_URL = "https://lk.sipli.ru:4011";
 
 export const WEB_PHONE_SOCKET = "wss://wrs.hanumi.net:8089/ws";
 export const WEB_PHONE_SOCKT_EU = "wss://wrtcger.hanumi.net:8089/ws";
